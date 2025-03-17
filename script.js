773692b00eac48ec84b1cbf9b30b54e5a38dc35e
shil-mil <shyshy122303@gmail.com>
2025-03-17 16:15:06 +0800

    Add bgm music

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const quote = document.getElementById("quote");
 const author = document.getElementById("author");
 const api_url = "https://strangerthingsquotes.shadowdev.xyz/api/quotes";
+let bgm = new Audio("strangerthings_bgm.mp3");
+bgm.loop = true;
 
 async function getQuote(url) {
     const response = await fetch(url);
     var data = await response.json();
     quote.innerHTML = data[0].quote;
     author.innerHTML = data[0].author;
+    bgm.play();
 }
 
 getQuote(api_url);
