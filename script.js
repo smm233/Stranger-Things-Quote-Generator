b0c8527c1aaa43fe3d890547b7dbed10e7a3bea1
shil-mil <shyshy122303@gmail.com>
2025-03-17 16:07:03 +0800

    Remove console.log

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const api_url = "https://strangerthingsquotes.shadowdev.xyz/api/quotes";
 async function getQuote(url) {
     const response = await fetch(url);
     var data = await response.json();
-    console.log(data);
     quote.innerHTML = data[0].quote;
     author.innerHTML = data[0].author;
 }
